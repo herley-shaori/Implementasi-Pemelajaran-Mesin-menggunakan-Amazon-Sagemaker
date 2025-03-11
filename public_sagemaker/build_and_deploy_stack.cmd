4ad72fcb4081e6cfd10cf1652d6639fa6b74743c
herley <herley.shaori@gmail.com>
2025-03-11 20:45:11 +0700

    Add CDK bootstrap step to build and deploy script.

--- public_sagemaker/build_and_deploy_stack.cmd
+++ public_sagemaker/build_and_deploy_stack.cmd
@@ -1,1 +1,1 @@
-mvn clean package & cdk acknowledge 32775 & cdk deploy --require-approval never --all
+mvn clean package & cdk acknowledge 32775 & cdk bootstrap & cdk deploy --require-approval never --all
